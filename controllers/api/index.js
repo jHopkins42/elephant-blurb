977bb67fdff380adff5ba63553d1aca681450929
jHopkins42 <jeremy.hopkins.547@outlook.com>
2023-05-04 23:00:32 -0400

    fonts

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -8,4 +8,14 @@ router.use('/users', userRoutes);
 router.use('/blogs', auth, blogRoutes);
 router.use('/comments', auth, commentRoutes);
 
+//font imports
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;1,100;1,300;1,400;1,500&display=swap');
+</style>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;1,100;1,300;1,400;1,500&display=swap" rel="stylesheet">
+
+</link>
+
 module.exports = router;
